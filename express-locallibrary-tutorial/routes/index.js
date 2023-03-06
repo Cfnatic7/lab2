619c4778f697a9ab0db6415ebdc37884f2049ec3
Cfnatic7 <78828448+Cfnatic7@users.noreply.github.com>
2023-03-06 12:04:47 +0100

    saving changes

--- express-locallibrary-tutorial/routes/index.js
+++ express-locallibrary-tutorial/routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var login = require('../controller/authenticate/login');
+
 let i = 0;
+
+let loggedIn = false;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,5 +21,34 @@ router.get('/', function(req, res, next) {
 router.get('/:name/:age', (req, res, next) => {
   res.send(`name: ${req.params.name}, age: ${req.params.age}`);
 })
+
+router.get('/login-form', (req, res, next) => {
+  res.render('sign-in-form', { 
+    error: false,
+    loggedIn
+   });
+})
+
+router.post('/login', function (req, res, next) {
+  const username = req.body.username;
+      let loginResult = login(username, req.body.password);
+  if (loginResult) {
+          loggedIn = true;
+          res.render('users', {username: username});
+      }
+      else {
+          loggedIn = false;
+          res.render('index', {error: true});
+      }
+  });
+
+router.get('/logout', (req, res, next) => {
+  loggedIn = false;
+  res.render('sign-in-form', { 
+    error: false,
+    loggedIn
+   });
+})
+
 
 module.exports = router;
